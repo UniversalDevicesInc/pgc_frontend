c16806035ea804b1cac402cd06002f0e5e596e26
Einsten42 <milne.james@gmail.com>
2019-01-14 20:31:31 -0600

    dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  PG_URI: 'https://spark.3csolutions.net',
+  PG_URI: 'https://pxu4jgaiue.execute-api.us-east-1.amazonaws.com/test',
   STORE_URI: 'https://hsl7p7mdal.execute-api.us-east-1.amazonaws.com/prod',
   STAGE: 'dev'
 }
