3f5777ed026e6dcee0cf61d55a606614fa815d6d
Einsten42 <milne.james@gmail.com>
2019-05-18 11:44:41 -0500

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  PG_URI: 'https://hsl7p7mdal.execute-api.us-east-1.amazonaws.com/prod',
+  PG_URI: 'https://lgpz727w3j.execute-api.us-east-1.amazonaws.com/prod',
   STORE_URI: 'https://hsl7p7mdal.execute-api.us-east-1.amazonaws.com/prod',
   STAGE: 'prod',
   PG_REDIRECT: '',
